7eb123276675289185db8cc2da60c0110864d75f
Trevor <truffini@student.bridgew.edu>
2021-02-16 00:41:48 -0500

    Sprint 2, adding database

--- APImain.py
+++ APImain.py
@@ -1,37 +1,82 @@
-import secrets
-import requests
+import sqlite3
 import math
+import requests
+import secrets
+
+url = f"https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2," \
+      f"3&fields=school.name,school.city,2018.student.size,2017.student.size," \
+      f"017.earnings.3_yrs_after_completion.overall_count_over_poverty_line," \
+      f"2016.repayment.3_yr_repayment.overall&api_key={secrets.api_key} "
+fields = ",".join([
+    "school.name",
+    "school.city",
+    "2018.student.size",
+    "2017.student.size",
+    "2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line",
+    "2016.repayment.3_yr_repayment.overall",
+])
+
+conn = sqlite3.connect("data.db")
 
 
 def get_data():
     all_data = []
-    response1 = requests.get(f"https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded"
-                             f".predominant=2,3&fields=school.name,school.city,2018.student.size,2017.student.size,"
-                             f"2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,"
-                             f"2016.repayment.3_yr_repayment.overall&api_key={secrets.api_key}")
-    page_of_data = response1.json()
+    response = requests.get(f"{url}&fields={fields}&api_key={secrets}&per_page={100}")
+    page_of_data = response.json()
     total_res = page_of_data['metadata']['total']
     items_per_page = page_of_data['metadata']['per_page']
-    print(page_of_data['metadata'])
-    for page in range(0, (math.floor(total_res / items_per_page) + 1)):
-        response = requests.get(f"https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded"
-                                f".predominant=2,3&fields=school.name,school.city,2018.student.size,"
-                                f"2017.student.size,"
-                                f"2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,"
-                                f"2016.repayment.3_yr_repayment.overall&api_key={secrets.api_key}&page={page}")
+
+    total_pages = math.ceil(total_res / items_per_page)
+    total_fetch_result = 0
+    for page in range(0, total_pages):
+        response = requests.get(f"{url}&fields={fields}&api_key={secrets}&page={page}&per_page={100}")
         if response.status_code != 200:
-            print("error with data")
+            print("Error with data syncing process")
             exit(-1)
+
         page_of_data = response.json()
-        page_of_school_data = page_of_data['results']
-        all_data.extend(page_of_school_data)
-        filename = open('Data.txt', 'a')
-        print(page_of_data, file=filename)
+        result = page_of_data['results']
+        print(f"Syncing Page {page + 1:02} of {total_pages} | {len(result):03} Data Items ")
+        for item in page_of_data['results']:
+            item_data = (
+                item["school.name"],
+                item["school.city"],
+                item["2017.student.size"],
+                item["2018.student.size"],
+                item["2016.repayment.3_yr_repayment.overall"],
+                item["2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line"]
+            )
+            total_fetch_result += 1
+            all_data.append(item_data)
+    print("[fetched items]: ", total_fetch_result)
+    return all_data
+
+
+def addtodatabase(all_data):
+    c = conn.cursor()
+    print("data count :", len(all_data))
+    c.execute('''CREATE TABLE IF NOT EXISTS school_data
+                            (
+                                school_name text,
+                                school_city text,
+                                student_size_2017 real,
+                                student_size_2018 real,
+                                earnings_3_yrs_after_completion_overall_count_over_poverty_line_2017 real,
+                                repayment_3_yr_repayment_overall_2016 real
+                            )'''
+              )
+
+    c.executemany('INSERT OR REPLACE INTO school_data VALUES (?,?,?,?,?,?)', all_data)
+    conn.commit()
+    c = conn.cursor()
+
+    c.execute('SELECT count(*) FROM school_data')
+    print(c.fetchone()[0], "Items inserted to the database")
 
 
 def main():
-    get_data()
-    print(get_data)
+    all_data = get_data()
+    addtodatabase(all_data)
 
 
 if __name__ == '__main__':
