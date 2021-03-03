acb48a248212b27d528629451c741245cabdd8de
Trevor <truffini@student.bridgew.edu>
2021-03-03 17:25:33 -0500

    che response

--- APImain.py
+++ APImain.py
@@ -26,6 +26,8 @@ conn = sqlite3.connect("data.db")
 def get_data():
     all_data = []
     response = requests.get(f"{url}&fields={fields}&per_page={100}")
+    if response.status_code != 200:
+        print(response.raw)
     page_of_data = response.json()
     total_res = page_of_data['metadata']['total']
     items_per_page = page_of_data['metadata']['per_page']
